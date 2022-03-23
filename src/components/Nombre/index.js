507f238dc893c5a7d9c08da66c714163c8644b26
Cecilia Rios Rodriguez <60232798+sailorEnana@users.noreply.github.com>
2022-03-22 19:00:57 -0700

    Esqueleto componente nombre

--- src/components/Nombre/index.js
+++ src/components/Nombre/index.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import "./styles.css";
 
-const index =()=>{
-    return(
+const index = () => {
+    return (
         <div>
-            <h2>soy index de nombre</h2>
+            <Container>
+                <h3>¿Cuál es tu nombre?</h3>
+                <Row>
+                    <Form.Control className="mb-3" type="text" placeholder="Nombre" />
+                    <Form.Control className="mb-3" type="text" placeholder="Segundo nombre" />
+                    <Form.Control className="mb-3" type="text" placeholder="Apellido paterno" />
+                    <Form.Control className="mb-3" type="text" placeholder="Apellido materno" />
+                </Row>
+            </Container>
         </div>
     );
 };
