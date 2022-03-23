acc39126ffd41b5cf00f9bf0b5fcfb5662558192
Cecilia Rios Rodriguez <60232798+sailorEnana@users.noreply.github.com>
2022-03-22 19:12:28 -0700

    Esqueleto componente datos

--- src/components/Datos/index.js
+++ src/components/Datos/index.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Container,Form,Row } from "react-bootstrap";
 import "./styles.css";
 
 const index=()=>{
     return(
         <div>
-            <h4>soy index de datos</h4>
+            <Container>
+                <h3>Datos de contacto</h3>
+                <Row>
+                    <Form.Control className="mb-3" type="email" placeholder="Correo electrónico"/>
+                    <Form.Control class="mb-3" type="number" placeholder="Teléfono celular"/>
+                </Row>
+            </Container>
         </div>
     );
 };
