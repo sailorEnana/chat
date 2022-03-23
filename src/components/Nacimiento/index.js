5c0b5b929127199f6063da1f95f2a926151b31c4
Cecilia Rios Rodriguez <60232798+sailorEnana@users.noreply.github.com>
2022-03-22 19:09:58 -0700

    Esqueleto componente nacimiento

--- src/components/Nacimiento/index.js
+++ src/components/Nacimiento/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Container, Form, Row } from "react-bootstrap";
 import "./styles.css";
 
-const index=()=>{
-    return(
+const index = () => {
+    return (
         <div>
-            <h3>soy index de nacimiento</h3>
+            <Container>
+                <h3>¿Cuál es tu fecha de nacimiento?</h3>
+                <Row>
+                    <Form.Control className="mb-3" type="number" placeholder="Día"/>
+                    <Form.Control className="mb-3" type="text" placeholder="Mes"/>
+                    <Form.Control className="mb-3" type="number" placeholder="Año"/>
+                </Row>
+            </Container>
         </div>
     );
 };
